6550520845015caafbfd7fc9337d41e140c59a78
Stanley Cao <daydream2013@live.com>
2020-03-23 11:10:54 +0800

    feat: responsive layout update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Anthurium') }}</title>
+    <title>@yield('title') - {{ config('app.name', 'Anthurium') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -26,11 +26,6 @@
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Anthurium') }}
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
 
@@ -40,52 +35,49 @@
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                        @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                        @endif
                         @else
-                            <li class="nav-item dropdown">
-            <a role="button" class="dropdown-toggle nav-link" id="dropdownMenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <span>
-                    @if(Auth::check() && Auth::user()->avatar_thumb_url)
-                        <img src="{{ Auth::user()->avatar_thumb_url }}" class="avatar-photo">
-                    @elseif(Auth::check() && Auth::user()->first_name && Auth::user()->last_name)
-                        <span class="avatar-initials">{{ mb_substr(Auth::user()->first_name, 0, 1) }}{{ mb_substr(Auth::user()->last_name, 0, 1) }}</span>
-                    @elseif(Auth::check() && Auth::user()->name)
-                        <span class="avatar-initials">{{ mb_substr(Auth::user()->name, 0, 1) }}</span>
-                    @elseif(Auth::guard(config('admin-auth.defaults.guard'))->check() && Auth::guard(config('admin-auth.defaults.guard'))->user()->first_name && Auth::guard(config('admin-auth.defaults.guard'))->user()->last_name)
-                        <span class="avatar-initials">{{ mb_substr(Auth::guard(config('admin-auth.defaults.guard'))->user()->first_name, 0, 1) }}{{ mb_substr(Auth::guard(config('admin-auth.defaults.guard'))->user()->last_name, 0, 1) }}</span>
-                    @else
-                        <span class="avatar-initials"><i class="fa fa-user"></i></span>
-                    @endif
+                        <li class="nav-item dropdown">
+                            <a role="button" class="dropdown-toggle nav-link" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <span>
+                                    @if(Auth::check() && Auth::user()->avatar_thumb_url)
+                                        <img src="{{ Auth::user()->avatar_thumb_url }}" class="avatar-photo">
+                                    @elseif(Auth::check() && Auth::user()->name)
+                                        <span class="avatar-initials">{{ mb_substr(Auth::user()->name, 0, 1) }}</span>
+                                    @elseif(Auth::guard(config('admin-auth.defaults.guard'))->check() && Auth::guard(config('admin-auth.defaults.guard'))->user()->first_name && Auth::guard(config('admin-auth.defaults.guard'))->user()->last_name)
+                                        <span class="avatar-initials">{{ mb_substr(Auth::guard(config('admin-auth.defaults.guard'))->user()->first_name, 0, 1) }}{{ mb_substr(Auth::guard(config('admin-auth.defaults.guard'))->user()->last_name, 0, 1) }}</span>
+                                    @else
+                                        <span class="avatar-initials"><i class="fa fa-user"></i></span>
+                                    @endif
 
-                        <span class="hidden-md-down">{{ Auth::user()->name }}</span>
+                                        <span class="d-none d-md-inline-block">{{ Auth::user()->name }}</span>
 
-                </span>
-                <span class="caret"></span>
-            </a>
+                                </span>
+                                <span class="caret"></span>
+                            </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" id="dropdownMenu">
-                                    <a class="dropdown-item" href="{{ route('profile') }}">{{ __('Profile') }}</a>
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+                            <div class="dropdown-menu dropdown-menu-right position-absolute p-0">
+                                <a class="dropdown-item" href="{{ route('profile') }}">{{ __('Profile') }}</a>
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                 document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
                         @endguest
                     </ul>
-                </div>
             </div>
         </nav>
 
