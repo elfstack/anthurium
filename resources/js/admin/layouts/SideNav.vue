ece494a2ef632a6d3add3704bc672523a68d918b
dodaydream <daydream2013@live.com>
2020-08-18 21:08:01 +0800

    feat: add form to side nav

--- resources/js/admin/layouts/SideNav.vue
+++ resources/js/admin/layouts/SideNav.vue
@@ -1,91 +1,97 @@
 <template>
-    <a-layout-sider
-        width="256"
-        v-model="collapsed"
-        collapsible
+  <a-layout-sider
+    width="256"
+    v-model="collapsed"
+    collapsible
+  >
+    <router-link
+      to="/"
+      tag="div"
+      class="logo h4 center" style="color: #fff;"
     >
-        <router-link
-            to="/"
-            tag="div"
-            class="logo h4 center" style="color: #fff;"
-        >
-            {{ !collapsed ? 'Anthurium' : 'An' }}
-        </router-link>
-        <a-menu
-            theme="dark"
-            :open-keys.sync="openKeys"
-            v-model="selectedKeys"
-            mode="inline"
-        >
-            <a-sub-menu>
-                <span slot="title"><a-icon type="user"/><span>Participants</span></span>
-                <a-menu-item key="users">
-                    <router-link to="/users">Users</router-link>
-                </a-menu-item>
-                <a-menu-item key="guests">
-                    <router-link to="/guests">Guests</router-link>
-                </a-menu-item>
-            </a-sub-menu>
-            <a-sub-menu key="activities">
-                <span slot="title"><a-icon type="schedule"/><span>Activities</span></span>
-                <a-menu-item key="activities">
-                    <router-link to="/activities">Activities</router-link>
-                </a-menu-item>
-            </a-sub-menu>
-            <a-sub-menu key="manage-access">
-                <span slot="title"><a-icon type="lock"/><span>Manage Access</span></span>
-                <a-menu-item key="admin-users" v-if="$can('admin.admin-users')">
-                    <router-link to="/manage-access/admin-users">Admin Users</router-link>
-                </a-menu-item>
-                <a-menu-item key="roles" v-if="$can('admin.roles')">
-                    <router-link to="/manage-access/roles">Roles & Permissions</router-link>
-                </a-menu-item>
-                <a-menu-item key="audits" v-if="$can('admin.audits')">
-                    <router-link to="/manage-access/audits">Action Log</router-link>
-                </a-menu-item>
-            </a-sub-menu>
+      {{ !collapsed ? 'Anthurium' : 'An' }}
+    </router-link>
+    <a-menu
+      theme="dark"
+      :open-keys.sync="openKeys"
+      v-model="selectedKeys"
+      mode="inline"
+    >
+      <a-sub-menu>
+        <span slot="title"><a-icon type="user"/><span>Participants</span></span>
+        <a-menu-item key="users">
+          <router-link to="/users">Users</router-link>
+        </a-menu-item>
+        <a-menu-item key="guests">
+          <router-link to="/guests">Guests</router-link>
+        </a-menu-item>
+      </a-sub-menu>
+      <a-sub-menu key="activities">
+        <span slot="title"><a-icon type="schedule"/><span>Activities</span></span>
+        <a-menu-item key="activities">
+          <router-link to="/activities">Activities</router-link>
+        </a-menu-item>
+      </a-sub-menu>
+      <a-sub-menu key="forms">
+        <span slot="title"><a-icon type="tool"/>Tools</span>
+        <a-menu-item key="forms">
+          <router-link to="/forms">Forms</router-link>
+        </a-menu-item>
+      </a-sub-menu>
+      <a-sub-menu key="manage-access">
+        <span slot="title"><a-icon type="lock"/><span>Manage Access</span></span>
+        <a-menu-item key="admin-users" v-if="$can('admin.admin-users')">
+          <router-link to="/manage-access/admin-users">Admin Users</router-link>
+        </a-menu-item>
+        <a-menu-item key="roles" v-if="$can('admin.roles')">
+          <router-link to="/manage-access/roles">Roles & Permissions</router-link>
+        </a-menu-item>
+        <a-menu-item key="audits" v-if="$can('admin.audits')">
+          <router-link to="/manage-access/audits">Action Log</router-link>
+        </a-menu-item>
+      </a-sub-menu>
 
-            <a-sub-menu key="site-settings" v-if="$can('admin.settings')">
-                <span slot="title"><a-icon type="setting"/><span>Site Settings</span></span>
-                <a-menu-item key="site-info">
-                    <router-link to="/site-settings/site-info">Site Info</router-link>
-                </a-menu-item>
-                <a-menu-item key="storage">
-                    <router-link to="/site-settings/storage">Manage Storage</router-link>
-                </a-menu-item>
-            </a-sub-menu>
-        </a-menu>
-    </a-layout-sider>
+      <a-sub-menu key="site-settings" v-if="$can('admin.settings')">
+        <span slot="title"><a-icon type="setting"/><span>Site Settings</span></span>
+        <a-menu-item key="site-info">
+          <router-link to="/site-settings/site-info">Site Info</router-link>
+        </a-menu-item>
+        <a-menu-item key="storage">
+          <router-link to="/site-settings/storage">Manage Storage</router-link>
+        </a-menu-item>
+      </a-sub-menu>
+    </a-menu>
+  </a-layout-sider>
 </template>
 
 <script>
-    export default {
-        name: "SideNav",
-        data() {
-            return {
-                openKeys: [],
-                selectedKeys: [],
-                collapsed: false
-            }
-        },
-        watch: {
-            'collapsed': function (val) {
-                if (val === true) {
-                    this.openKeys = []
-                }
-            }
-        },
-        created() {
-            const name = this.$route.name.split('.')
-            this.openKeys = [name[1]]
-            this.selectedKeys = [name[2]]
+  export default {
+    name: "SideNav",
+    data() {
+      return {
+        openKeys: [],
+        selectedKeys: [],
+        collapsed: false
+      }
+    },
+    watch: {
+      'collapsed': function (val) {
+        if (val === true) {
+          this.openKeys = []
         }
+      }
+    },
+    created() {
+      const name = this.$route.name.split('.')
+      this.openKeys = [name[1]]
+      this.selectedKeys = [name[2]]
     }
+  }
 </script>
 
 <style scoped>
-    .logo {
-        height: 64px;
-        line-height: 64px;
-    }
+  .logo {
+    height: 64px;
+    line-height: 64px;
+  }
 </style>
